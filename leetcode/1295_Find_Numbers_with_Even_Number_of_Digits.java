2a7ff2c9c01ebefe5dde276368f55a9c549f98f2
naw <na.wei@citrix.com>
2020-01-21 15:47:41 +0800

    1295_Find_Numbers_with_Even_Number_of_Digits

--- leetcode/1295_Find_Numbers_with_Even_Number_of_Digits.java
+++ leetcode/1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -1,5 +1,12 @@
 class Solution {
     public int findNumbers(int[] nums) {
-        
+        int ret = 0;
+        for (int i=0; i< nums.length; i++){
+            String str = Integer.toString(nums[i]);
+            if(str.length()%2 == 0){
+                ret++;
+            }
+        }
+        return ret;
     }
 }
