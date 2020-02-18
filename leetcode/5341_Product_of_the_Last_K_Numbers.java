d466396a743b280334d22a551cf2fd14648be79e
naw <na.wei@citrix.com>
2020-02-18 14:10:57 +0800

    5340, 5341 and 5342

---
+++ leetcode/5341_Product_of_the_Last_K_Numbers.java
@@ -0,0 +1,25 @@
+class ProductOfNumbers {
+    ArrayList<Integer> product;
+    public ProductOfNumbers() {
+        this.product = new ArrayList<Integer>();
+    }
+    
+    public void add(int num) {
+        this.product.add(num);
+    }
+    
+    public int getProduct(int k) {
+        int ret = 1;
+        for (int i=this.product.size()-1; i>=this.product.size()-k; i--){
+            ret = ret * this.product.get(i);
+        }
+        return ret;
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers obj = new ProductOfNumbers();
+ * obj.add(num);
+ * int param_2 = obj.getProduct(k);
+ */
